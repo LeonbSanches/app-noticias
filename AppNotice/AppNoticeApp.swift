5b370c86a97bce4dded05a217e8aae25047d0c4b
leonardo <leonardo-duarte@bm.rs.gov.br>
2026-02-23 13:00:12 -0300

    Initial commit

---
+++ AppNotice/AppNoticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppNoticeApp.swift
+//  AppNotice
+//
+//  Created by CPINfo on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AppNoticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
