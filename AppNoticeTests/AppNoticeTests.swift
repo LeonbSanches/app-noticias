4d0248508484b47b23ea52ec1c4f8555fa2777d6
leonardo <leonardo-duarte@bm.rs.gov.br>
2026-03-27 15:50:53 -0300

    Etapa inicial de testes initarios

---
+++ AppNoticeTests/AppNoticeTests.swift
@@ -0,0 +1,16 @@
+//
+//  AppNoticeTests.swift
+//  AppNoticeTests
+//
+//  Created by CPINfo on 27/03/26.
+//
+
+import Testing
+
+struct AppNoticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
